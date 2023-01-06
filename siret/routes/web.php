cad84125862c997b8d59430df57ea327087fe834
Korobetski Sigfrid <lunaticchimera@gmail.com>
2023-01-06 11:16:12 +0100

    ajout d'un fallback 404

--- siret/routes/web.php
+++ siret/routes/web.php
@@ -13,3 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::view('/swagger', 'swagger');
+
+Route::fallback(function () {
+    return [
+        'statut' => 404,
+        'error' => "Not Found",
+    ];
+});
